eb0b1b41ac39b2f56e576caa2611e40ec6d52383
xuwenxiang <164942747@qq.com>
2019-12-13 17:03:58 +0800

    1.add df function

--- translaterservice/sever_manger.py
+++ translaterservice/sever_manger.py
@@ -14,25 +14,33 @@ class MemUsage:
     available = ""
 
 
+UTF_8 = 'utf-8'
+
+
 def getMemUsage(request):
     return JsonResponse(Result(RESULT_SUCCESS, "成功获取").getJsonStr(getVpsCacheUsage()))
 
 
 def getCpuUsuage(request):
-    result = subprocess.run(['mpstat'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result = subprocess.run(['mpstat'], stdout=subprocess.PIPE).stdout.decode(UTF_8).splitlines()
     return JsonResponse(Result(RESULT_SUCCESS, "成功获取").getJsonStr(result))
 
 
 def ps(request):
-    result = subprocess.run(['ps', '-ejf'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result = subprocess.run(['ps', '-ejf'], stdout=subprocess.PIPE).stdout.decode(UTF_8).splitlines()
     return JsonResponse(Result(RESULT_SUCCESS, "成功获取").getJsonStr(result))
 
 
 def last(request):
-    result = os.popen('last | grep "logged in"').read()
+    result = os.popen('last | grep "logged in"').read().splitlines()
+    return JsonResponse(Result(RESULT_SUCCESS, "成功获取").getJsonStr(result))
+
+
+def df(request):
+    result = subprocess.run(['df', '-h'], stdout=subprocess.PIPE).stdout.decode(UTF_8).splitlines()
     return JsonResponse(Result(RESULT_SUCCESS, "成功获取").getJsonStr(result))
 
 
 def getVpsCacheUsage():
-    result = subprocess.run(['free', '-h'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    result = subprocess.run(['free', '-h'], stdout=subprocess.PIPE).stdout.decode(UTF_8).splitlines()
     return result
